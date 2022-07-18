8234ed9977d992ea9c5cdfba1908a8372708559e
ashishnair436 <ashishnair436@gmail.com>
2022-07-19 04:23:46 +0530

    Initial Commit

---
+++ AR Resume Testing/Assets/Scripts/LinkManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OpenLinkedIn()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/ashish-nair436/");
+    }
+
+    public void OpenInstagram()
+    {
+        Application.OpenURL("https://www.instagram.com/ashish_nair_95/");
+    }
+    public void OpenDiscord()
+    {
+        Application.OpenURL("https://discordapp.com/users/614745571449896962/");
+    }
+    public void OpenGithub()
+    {
+        Application.OpenURL("https://github.com/ashishnair436");
+    }
+
+    public void OpenWhatsapp()
+    {
+        Application.OpenURL("https://wa.me/918149705495");
+    }
+
+
+    public void Open3DMiniMilitia()
+    {
+        Application.OpenURL("https://github.com/ashishnair436/onlineshootergame");
+    }
+
+    public void OpenSpaceShooter()
+    {
+        Application.OpenURL("https://github.com/ashishnair436/Space-Shooter-Pro-LFS");
+    }
+
+    public void OpenResume()
+    {
+        Application.OpenURL("https://drive.google.com/file/d/1HWnc7inUPThFxWt3ppQFP_fhIqmegzlQ/view?usp=sharing");
+    }
+    
+    public void OpenHighTime()
+    {
+        Application.OpenURL("https://github.com/yatharthgupta7/HIgh-TIme");
+    }
+}
